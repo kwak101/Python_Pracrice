3448fb3f4b597c984ffc302e3209a9386ad00195
kwak101 <kwak101>
2017-06-22 16:48:34 +0900

    5th commit

--- tensor_flow_practice.py
+++ tensor_flow_practice.py
@@ -140,8 +140,75 @@ def mnist_simple_nn(in_lR, in_iN):
     rst = sess.run(accuracy, feed_dict={x:mnist.test.images, y_t: mnist.test.labels})
     print (rst)
 
-def mnist_cnn(in_lR, in_iN)
+def wgt_variable(shape):
+    init = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(init)
 
+def bias_variable(shape):
+    init = tf.constant (0.1, shape=shape)
+    return tf.Variable(init)
+
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding = 'SAME')
+
+def max_pooling_2x2 (x):
+    return tf.nn.max_pool (x, ksize=[1,2,2,1], strides=[1,2,2,1], padding ="SAME")
+
+def mnist_cnn(in_Ap, in_iN):
+
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+    x = tf.placeholder("float", shape = [None, 784]) # holds input image data  (number is undefined)
+    y_t = tf.placeholder("float", shape= [None, 10]) # holds answer set (number is undefined)
+
+    x_img = tf.reshape(x, [-1,28,28,1])
+    print ("x_image={0}".format(x_img))
+
+    W_conv1 = wgt_variable([5,5,1,32]) # 32 is current filter num
+    b_conv1 = bias_variable([32])
+
+    h_conv1 = tf.nn.relu(conv2d(x_img, W_conv1) + b_conv1)
+    h_pool1 = max_pooling_2x2(h_conv1)
+
+    W_conv2 = wgt_variable([5,5,32,64]) # 32 is previous filter num, as current channel num. 64 is current filter num
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pooling_2x2(h_conv2)
+
+    W_fc1 = wgt_variable([7*7*64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1)+ b_fc1)
+
+    keep_prob = tf.placeholder("float") # connect to keeping probability for drop-out
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = wgt_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    cross_entropy = -tf.reduce_sum(y_t * tf.log(y_conv))
+    train_step = tf.train.AdamOptimizer(in_Ap).minimize(cross_entropy)
+    correct_precision = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_t, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_precision, "float"))
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    for i in range (in_iN):
+        batch = mnist.train.next_batch(100)
+        if i%100 == 0:
+            train_accuracy = sess.run(accuracy, feed_dict={x:batch[0], y_t:batch[1], keep_prob:1.0})
+            print ("step {0}, training accuracy: {1}%".format(i, train_accuracy))
+        sess.run(train_step, feed_dict={x:batch[0], y_t:batch[1], keep_prob:0.5})
+
+    print("test accuracy: {0}".format(sess.run(accuracy,
+         feed_dict={x:mnist.test.images, y_t:mnist.test.labels, keep_prob:1.0})))
 
 
 def main():
@@ -149,8 +216,8 @@ def main():
     #xy = preprare_data(0.1, 0.3, 2000, 'b')
     #linear_regress(xy, 100)
     #k_means(xy, 4, 100)
-    mnist_simple_nn(0.01, 1000:memoryview)
-
+    #mnist_simple_nn(0.01, 1000)
+    mnist_cnn(1e-4, 1000)
 
 if __name__ == "__main__":
     main()
